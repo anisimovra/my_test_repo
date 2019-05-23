5a8242f32309378fd0af3b22ca05b4c235b69037
Roman Anisimov <romakms@yandex.ru>
2019-05-23 16:35:18 +0300

    First files for git testing

---
+++ Documents/start_service.bat
@@ -0,0 +1,8 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+rem ---------------------------------------------------------------------------
+rem Start script for the Bitbucket and Bitbucket Elasticsearch services
+rem
+
+net start AtlassianBitbucketElasticsearch
+net start AtlassianBitbucket
